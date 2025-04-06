51ad629f4e64c470ea613ebc386add8793848202
Bright Nkrumah <brytey2k@gmail.com>
2025-04-06 11:29:13 +0000

    chore: change run interval for podcast cache pruning

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 Illuminate\Support\Facades\Schedule::command('app:cache-podcast-content')
     ->everyTwoMinutes();
 Illuminate\Support\Facades\Schedule::command('app:prune-stale-cached-podcast-content')
-    ->everyThirtyMinutes();
+    ->everyFifteenMinutes();
