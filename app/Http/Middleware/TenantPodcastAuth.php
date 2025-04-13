a2e1f475b9fd3b537cdb16a80916661ae7d2607c
Bright Nkrumah <brytey2k@gmail.com>
2025-04-13 11:55:01 +0000

    chore: fix code styling

--- app/Http/Middleware/TenantPodcastAuth.php
+++ app/Http/Middleware/TenantPodcastAuth.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -16,7 +18,8 @@ class TenantPodcastAuth
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(Request): (Response)  $next
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
